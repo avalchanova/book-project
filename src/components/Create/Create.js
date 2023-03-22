0b6c85df1f098fc142b4f6d77e43349f186ad385
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-03-22 15:09:28 +0200

    optimized create form

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -4,60 +4,77 @@ const Create = () => {
     return (
         <form className="row g-3" style={styles.form}>
             <div className="col-md-6 p3">
-                <label for="inputTitlel4" className="form-label">Title</label>
-                <input type="text" className="form-control" id="inputTitle4" />
+                <label htmlFor="inputTitlel4" className="form-label">Title</label>
+                <input type="text" className="form-control" id="inputTitle4" placeholder="Little Women" />
             </div>
             <div className="col-md-6">
-                <label for="inputAuthor4" className="form-label">Author</label>
-                <input type="text" className="form-control" id="inputAuthor4" />
+                <label htmlFor="inputAuthor4" className="form-label">Author</label>
+                <input type="text" className="form-control" id="inputAuthor4" placeholder="Louisa May Alcott" />
             </div>
             <div className="col-12">
-                <label for="inputImageUrl" className="form-label">Image URL</label>
+                <label htmlFor="inputImageUrl" className="form-label">Image URL</label>
                 <input type="text" className="form-control" id="inputImageUrl" placeholder="https://" />
             </div>
             <div className="col-12">
-                <label for="inputSummary" className="form-label">Summary</label>
+                <label htmlFor="inputSummary" className="form-label">Summary</label>
                 <input type="text" className="form-control" id="inputSummary" placeholder="" />
             </div>
             <div className="col-12">
-                <label for="inputState" className="form-label">Genre</label>
+                <label htmlFor="inputState" className="form-label">Genre</label>
                 <select id="inputState" className="form-select">
                     <option selected>Choose...</option>
-                    <option>...</option>
+                    <option value="adventure">Adventure</option>
+                    <option value="childrensFiction">Children's Fiction</option>
+                    <option value="dystopian">Dystopian</option>
+                    <option value="fantasy">Fantasy</option>
+                    <option value="historicalFiction">Historical Fiction</option>
+                    <option value="horror">Horror</option>
+                    <option value="mystery">Mystery</option>
+                    <option value="romance">Romance</option>
+                    <option value="scienceFiction">Science Fiction</option>
+                    <option value="thriller">Thriller</option>
+                    <option value="youngAdult">Young Adult</option>
+                    <option value="nonfiction">Nonfiction</option>
+                    <option value="other">Other</option>
                 </select>
             </div>
             <div className="col-12">
-                <label for="inputFavouriteQuote" className="form-label">Favourite Quote</label>
-                <input type="text" className="form-control" id="inputFavouriteQuote" placeholder="" />
+                <label htmlFor="inputFavouriteQuote" className="form-label">Favourite Quote</label>
+                <input type="text" className="form-control" id="inputFavouriteQuote" placeholder="Conceit spoils the finest genius." />
+            </div>
+            <div className="col-12">
+                <label htmlFor="inputFavouriteQuote" className="form-label">Year of Publication</label>
+                <input type="text" className="form-control" id="inputFavouriteQuote" placeholder="1868" />
             </div>
 
             {/* <div className="col-12">
-                <label for="inputGenre" className="form-label">Genre</label>
+                <label htmlFor="inputGenre" className="form-label">Genre</label>
                 <input type="text" className="form-control" id="inputGenre" placeholder="Romance" />
             </div> */}
             <div className="col-md-6">
-                <label for="inputTropes" className="form-label">Tropes</label>
+                <label htmlFor="inputTropes" className="form-label">Tropes</label>
                 <input type="text" className="form-control" id="inputTropes" />
             </div>
             <div className="col-md-6">
-                <label for="inputPages" className="form-label">Pages</label>
+                <label htmlFor="inputPages" className="form-label">Pages</label>
                 <input type="text" className="form-control" id="inputPages" />
             </div>
             <div className="col-12" >
                 <div className="form-check">
                     <input className="form-check-input" type="checkbox" id="gridCheck" />
-                    <label class="form-check-label" for="gridCheck" style={styles.radioButtonTxt}>Part of a series</label>
+                    <label class="form-check-label" htmlFor="gridCheck" style={styles.radioButtonTxt}>Part of a series</label>
                 </div>
             </div>
-            <div className="col-12" >
+            <div className="col-12" style={styles.buttonContainer}>
                 <button type="submit" className="btn" style={styles.button}>Create Post</button>
             </div>
+
         </form>
     );
 };
 const styles = {
     form: {
-        paddingTop: "50px",
+        paddingTop: "1.5%",
         paddingLeft: "35%",
         paddingRight: "35%"
     },
@@ -67,6 +84,10 @@ const styles = {
     },
     radioButtonTxt: {
         textAlign: "center",
+    },
+    buttonContainer: {
+        justifyContent: 'center',
+        display: 'flex',
     }
 };
 
