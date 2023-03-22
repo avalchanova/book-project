0fc5114d40423d073bc6de6bf69637ef741a7fc1
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-03-22 15:10:03 +0200

    added login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,83 +1,44 @@
 import React from 'react';
 
 const Login = () => {
-    // return (
-    //     <div>
-    //         <div className="wrapper">
-    //             <div className="title-text">
-    //                 <div className="title login">
-    //                     Login Form
-    //                 </div>
-    //                 <div className="title signup">
-    //                     Signup Form
-    //                 </div>
-    //             </div>
-    //             <div className="form-container">
-    //                 <div className="slide-controls">
-    //                     <input type="radio" name="slide" id="login" checked />
-    //                     <input type="radio" name="slide" id="signup" />
-    //                     <label for="login" className="slide login">Login</label>
-    //                     <label for="signup" className="slide signup">Signup</label>
-    //                     <div className="slider-tab"></div>
-    //                 </div>
-    //                 <div className="form-inner">
-    //                     <form action="#" className="login">
-    //                         <div className="field">
-    //                             <input type="text" placeholder="Email Address" required />
-    //                         </div>
-    //                         <div className="field">
-    //                             <input type="password" placeholder="Password" required />
-    //                         </div>
-    //                         <div className="pass-link">
-    //                             <a href="#">Forgot password?</a>
-    //                         </div>
-    //                         <div className="field btn">
-    //                             <div className="btn-layer"></div>
-    //                             <input type="submit" value="Login" />
-    //                         </div>
-    //                         <div className="signup-link">
-    //                             Not a member? <a href="">Signup now</a>
-    //                         </div>
-    //                     </form>
-    //                     <form action="#" className="signup">
-    //                         <div className="field">
-    //                             <input type="text" placeholder="Email Address" required />
-    //                         </div>
-    //                         <div className="field">
-    //                             <input type="password" placeholder="Password" required />
-    //                         </div>
-    //                         <div className="field">
-    //                             <input type="password" placeholder="Confirm password" required />
-    //                         </div>
-    //                         <div className="field btn">
-    //                             <div className="btn-layer"></div>
-    //                             <input type="submit" value="Signup" />
-    //                         </div>
-    //                     </form>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //         {/* <script>
-    //    const loginText = document.querySelector(".title-text .login");
-    //    const loginForm = document.querySelector("form.login");
-    //    const loginBtn = document.querySelector("label.login");
-    //    const signupBtn = document.querySelector("label.signup");
-    //    const signupLink = document.querySelector("form .signup-link a");
-    //    signupBtn.onclick = (()=>{
-    //      loginForm.style.marginLeft = "-50%";
-    //      loginText.style.marginLeft = "-50%";
-    //    });
-    //    loginBtn.onclick = (()=>{
-    //      loginForm.style.marginLeft = "0%";
-    //      loginText.style.marginLeft = "0%";
-    //    });
-    //    signupLink.onclick = (()=>{
-    //      signupBtn.click();
-    //      return false;
-    //    });
-    // </script> */}
-    //     </div>
-    // );
+    return (
+        <form style={styles.form}>
+            <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" />
+            </div>
+            {/* <div className="mb-3 form-check">
+                <input type="checkbox" className="form-check-input" id="exampleCheck1" /> */}
+            {/* <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label> */}
+            {/* </div> */}
+            <div className="signup-link">
+                Not a member? <a href="/register">Register now</a>
+            </div>
+            <div style={styles.buttonContainer}>
+                <button type="submit" className="btn btn-primary" style={styles.button}>Login</button>
+            </div>
+        </form>
+    );
+};
+const styles = {
+    form: {
+        paddingTop: "5%",
+        paddingLeft: "40%",
+        paddingRight: "40%"
+    },
+    button: {
+        backgroundColor: "#ffe45e",
+        border: 'none',
+    },
+    buttonContainer: {
+        paddingTop: '10px',
+        justifyContent: 'center',
+        display: 'flex',
+    }
 };
 
 export default Login;
