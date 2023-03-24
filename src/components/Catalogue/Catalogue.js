19ac2049ea4ac82b6d42a78090974053e1aa4e44
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-03-24 17:29:51 +0200

    mapping through the book posts and rendering each catalogueItem in the catalogue

--- src/components/Catalogue/Catalogue.js
+++ src/components/Catalogue/Catalogue.js
@@ -1,107 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-// TODO: center the card-container
-const Catalogue = () => {
+import CatalogueItem from './CatalogueItem/CatalogueItem.js';
+
+const Catalogue = ({ books }) => {
     return (
         <section id="catalogue-page">
             <h1 style={styles.heading}>All Posts</h1>
             <div id="card-container" style={styles.cardContainer}>
-                <div className="card border-3 rounded-3 mb-3" style={styles.card}>
-                    <div className="row g-0">
-                        <div className="col-md-4">
-                            <img src="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/91l1EHFs8dL.jpg" className="img-fluid rounded-start" alt="..." />
-                        </div>
-                        <div className="col-md-8">
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <div>
-                                    <Link to="/details" className="btn btn-primary" style={styles.button}>Details</Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="card border-3 rounded-3 mb-3" style={styles.card}>
-                    <div className="row g-0">
-                        <div className="col-md-4">
-                            <img src="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/91l1EHFs8dL.jpg" className="img-fluid rounded-start" alt="..." />
-                        </div>
-                        <div className="col-md-8">
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <div>
-                                    <Link to="/details" className="btn btn-primary" style={styles.button}>Details</Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div><div className="card border-3 rounded-3 mb-3" style={styles.card}>
-                    <div className="row g-0">
-                        <div className="col-md-4">
-                            <img src="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/91l1EHFs8dL.jpg" className="img-fluid rounded-start" alt="..." />
-                        </div>
-                        <div className="col-md-8">
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <div>
-                                    <Link to="/details" className="btn btn-primary" style={styles.button}>Details</Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="card border-4 rounded-3 mb-3" style={styles.card}>
-                    <div className="row g-0">
-                        <div className="col-md-4">
-                            <img src="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/91l1EHFs8dL.jpg" className="img-fluid rounded-start" alt="..." />
-                        </div>
-                        <div className="col-md-8">
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <div>
-                                    <Link to="/details" className="btn btn-primary" style={styles.button}>Details</Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                {books.map(x =>
+                    <CatalogueItem key={x._id} {...x} />
+                )}
+                {books.length === 0 && (
+                    <h3 className="no-articles">No posts yet</h3>
+                )}
             </div>
-            {/* TODO: posts */}
-            {/* {posts.map(x=>)} */}
         </section>
     );
 };
+
 const styles = {
     heading: {
         paddingTop: "5%",
-        // paddingBottom: "2%",
         textAlign: "center"
-    },
-    card: {
-        maxWidth: "540px",
-        borderColor: "#ffe45e",
-        display: "inline-block",
-        justifyContent: "space-between",
-        margin: '10px',
-        boxShadow: "1px 2px 15px 1px rgba(20, 20, 20, 0.1)"
     },
     cardContainer: {
         padding: "5%",
-        // paddingLeft: "5%",
-        // paddingRight: "5%",
         marginLeft: "auto",
         marginRight: "auto",
-        // margin: "auto",
-        // width: "60%",
     },
-    button: {
-        backgroundColor: "#ffe45e",
-        border: 'none',
-    }
-
 };
 export default Catalogue;
--- src/components/Catalogue/CatalogueItem/CatalogueItem.js
+++ src/components/Catalogue/CatalogueItem/CatalogueItem.js
@@ -1,22 +1,45 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
-const CatalogItem = () => {
+
+const CatalogueItem = ({
+    _id,
+    title,
+    imageUrl,
+    summary
+}) => {
     return (
-        <div className="card mb-3" style="max-width: 540px;">
+        <div className="card border-3 rounded-3 mb-3" style={styles.card}>
             <div className="row g-0">
                 <div className="col-md-4">
-                    <img src="..." className="img-fluid rounded-start" alt="..." />
+                    <img src={imageUrl} className="img-fluid rounded-start" alt="bookCover" />
                 </div>
                 <div className="col-md-8">
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                        <h4 className="card-title">{title}</h4>
+                        <p className="card-text">{summary}</p>
+                        <div>
+                            <Link to={`/catalog/${_id}`} className="btn btn-primary" style={styles.button}>Details</Link>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     );
 };
+const styles = {
+    card: {
+        // TODO: cards are a little wobbly in size and are not in the line and go a little zigzaggy
+        width: "540px",
+        height: "280px",
+        borderColor: "#ffe45e",
+        display: "inline-block",
+        justifyContent: "space-between",
+        margin: '10px',
+        boxShadow: "1px 2px 15px 1px rgba(20, 20, 20, 0.1)"
+    }, button: {
+        backgroundColor: "#ffe45e",
+        border: 'none',
+    }
+};
 
-export default CatalogItem;
+export default CatalogueItem;
