0dafb7f02ba9c7466cefff7871feb21466bd5fa1
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-03-28 16:33:37 +0300

    added token to the requester

--- src/services/requester.js
+++ src/services/requester.js
@@ -1,7 +1,8 @@
-const request = async (method, url, data) => {
+const request = async (method, token, url, data) => {
     const options = {};
 
     if (method !== "GET") {
+        //options are an empty object if the method is GET because the fetch does not need any data, only to URL to fetch a get request
         options.method = method;
         if (data) {
             options.headers = {
@@ -10,19 +11,35 @@ const request = async (method, url, data) => {
             options.body = JSON.stringify(data);
         }
     }
+    if (token) {
+        options.headers = {
+            ...options.headers, // destructuring helps us save the other headers and add a new property
+            'X-Authorization': token,
+        };
+    }
 
     const response = await fetch(url, options);
-    try {
-        const result = await response.json();
-        return result;
-    } catch (error) {
-        return {};
-        // will return the result, but if the result is undefined, it will return {}
-    }
-};
 
-export const get = request.bind(null, "GET");
-export const post = request.bind(null, "POST");
-export const put = request.bind(null, "PUT");
-export const del = request.bind(null, "DELETE");
+    if (response.status === 204) {
+        return {};
+    }
+
+    const result = await response.json();
+
+    if (!response.ok) {
+        throw result;
+    }
+    return result;
+
+};
+// Factory function is a function which will create an object
+export const requestFactory = (token) => {
+    return {
+        get: request.bind(null, "GET", token),
+        post: request.bind(null, "POST", token),
+        put: request.bind(null, "PUT", token),
+        delete: request.bind(null, "DELETE", token),
+        patch: request.bind(null, "PATCH", token),
+    };
+}
 // export const patch = request.bind(null, "PATCH");
