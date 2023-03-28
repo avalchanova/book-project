9213f7dee324161b3aaa2eb68574236c54d8aa29
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-03-28 16:31:29 +0300

    book services like create, edit, delete, etc

--- src/services/bookService.js
+++ src/services/bookService.js
@@ -1,19 +1,40 @@
-import * as request from './requester.js';
+import { requestFactory } from './requester.js';
 
-const baseUrl = `http://localhost:3030/jsonstore/books`;
+const baseUrl = `http://localhost:3030/data/books`;
 
-export const getAll = async () => {
-    const result = await request.get(baseUrl);
-    const books = Object.values(result);
-    return books;
-};
+export const bookServiceFactory = (token) => {
+    const request = requestFactory(token);
+    const getAll = async () => {
+        const result = await request.get(baseUrl);
+        const books = Object.values(result); //we are modifying the data here because the result comes as an {} with ID and the value of the ID is the whole content of the game = name, description, categories
+        return books;
+    };
 
-export const create = async (bookData) => {
-    const result = await request.post(baseUrl, bookData);
-    return result;
-};
+    const getOne = async (bookId) => {
+        const result = await request.get(`${baseUrl}/${bookId}`);
+        return result;
+    };
 
-export const getOne = async (bookId) => {
-    const result = await request.get(`${baseUrl}/${bookId}`);
-    return result;
+    const create = async (bookData) => {
+        const result = await request.post(baseUrl, bookData);
+        return result;
+    };
+
+    const edit = (bookId, data) => request.put(`${baseUrl}/${bookId}`, data);
+
+    const deleteGame = (gameId) => request.delete(`${baseUrl}/${gameId}`);
+
+    const addComment = async (bookId, data) => {
+        const result = await request.post(`${baseUrl}/${bookId}/comments`, data);
+        return result;
+    };
+
+    return {
+        getAll,
+        getOne,
+        create,
+        edit,
+        deleteGame,
+        addComment,
+    };
 };
