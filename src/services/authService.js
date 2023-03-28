7ad0c095fe481a2ee6e636d959f2d1ab74b5b192
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-03-28 16:30:51 +0300

    login, register, logout in service

---
+++ src/services/authService.js
@@ -0,0 +1,15 @@
+import { requestFactory } from "./requester.js";
+
+const baseUrl = `http://localhost:3030/users`;
+
+export const authServiceFactory = (token) => {
+    const request = requestFactory(token);
+
+    return {
+        login: (data) => request.post(`${baseUrl}/login`, data),
+        register: (data) => request.post(`${baseUrl}/register`, data),
+        //if we have any other data like first name, last name, etc. must save it
+        //in the payload of the request and it will be saved
+        logout: () => request.get(`${baseUrl}/logout`)
+    };
+};
