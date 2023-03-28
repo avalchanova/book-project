05130a64601170a0310b2d7e82eb94892c724f56
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-03-28 16:45:09 +0300

    divided navigation for logged-in users and for guests

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
-import React from 'react';
-// import styles from './Navigation.module.css';
+// TODO CSS: import styles from './Navigation.module.css';
+import { AuthContext } from '../../contexts/AuthContext.js';
 import { NavLink } from 'react-router-dom';
+import { useContext } from 'react';
 import styles from './Header.module.css';
 
 
 const Header = () => {
+    // TODO: add first name of the logged in user in the header
+    const { isAuthenticated } = useContext(AuthContext);
     return (
         <nav className={styles.navigation}>
             <nav className="navbar navbar-expand-lg">
@@ -13,6 +16,7 @@ const Header = () => {
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
+
                     <div className="collapse navbar-collapse" id="navbarText">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
@@ -21,21 +25,27 @@ const Header = () => {
                             <li className="nav-item">
                                 <NavLink className="nav-link" to="/catalogue">Catalogue</NavLink>
                             </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/create">Create</NavLink>
-                            </li>
+                            {/* Logged-in Users */}
+                            {isAuthenticated && (
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to="/create">Create</NavLink>
+                                </li>
+                            )}
                         </ul>
                         <ul className="navbar-nav mb-2 mb-lg-0 navbar-right">
 
                             <li className="nav-item" id="login">
                                 <NavLink className="nav-link" to="/login">Login</NavLink>
                             </li>
-                            <li className="nav-item" id="logout">
-                                <NavLink className="nav-link" to="/logout">Logout</NavLink>
-                            </li>
                             <li className="nav-item" id="register">
                                 <NavLink className="nav-link" to="/register">Register</NavLink>
                             </li>
+                            {/* Logged-in Users */}
+                            {isAuthenticated && (
+                                <li className="nav-item" id="logout">
+                                    <NavLink className="nav-link" to="/logout">Logout</NavLink>
+                                </li>
+                            )}
                         </ul>
                         <div className="navbar-nav navbar-right mb-2 mb-lg-0" id="navBarAuth">
                         </div>
