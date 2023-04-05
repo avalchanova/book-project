c9460353c4c9b8a5c77df9a2780d174dd3b4a70c
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-04-05 18:48:29 +0300

    connected localStorage with authContext

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,3 +1,79 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
+// AuthContext is just a context which only purpose is to pass something to the children
+import { useNavigate } from "react-router-dom";
+import { useLocalStorage } from "../hooks/useLocalStorage.js";
 
+import { authServiceFactory } from "../services/authService.js";
+
+// creating context
 export const AuthContext = createContext();
+// this is a component/ something like a wrapper component
+export const AuthProvider = ({
+    children,
+    //in props there is always a property "children"
+}) => {
+    const [auth, setAuth] = useLocalStorage('auth', {});
+    const navigate = useNavigate();
+    // this state has to persist in the local storage because when refreshed the user is automatically logged out
+    // react does not have a close connection with the local storage so it does not know when there is a change
+
+    const authService = authServiceFactory(auth.accessToken);
+
+    const onLoginSubmit = async (data) => {
+        try {
+            const result = await authService.login(data);
+            setAuth(result);
+            navigate('catalogue');
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+    const onRegisterSubmit = async (values) => {
+        const { repeatPassword, ...registerData } = values;
+        // by destructuring we create a new obj registerData which will exclude repeatPassword
+
+        if (repeatPassword !== registerData.password) {
+            return; // TODO: throw error for user
+        }
+        try {
+            const result = await authService.register(registerData);
+            setAuth(result);
+            navigate('/catalogue');
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
+    const onLogout = async () => {
+        await authService.logout(); //will logout on the server
+        setAuth({}); //will logout on the client
+        localStorage.clear(); //clears the auth
+        navigate('/');
+    };
+
+
+    const contextValues = {
+        onLoginSubmit,
+        onRegisterSubmit,
+        onLogout,
+        userId: auth._id,
+        token: auth.accessToken,
+        name: auth.firstName,
+        isAuthenticated: !!auth.acessToken
+        // this is double negation
+        // it returns each truthy value in true and each falsy value in false
+        // for instance a string "afvjd" will be converted in false and then finally in true
+    };
+    // console.log(contextValues.firstName);
+
+    return (
+        <AuthContext value={contextValues}>
+            {children}
+        </AuthContext>
+    );
+};
+
+export const useAuthContext = () => {
+    const context = useContext(AuthContext);
+    return context;
+};
