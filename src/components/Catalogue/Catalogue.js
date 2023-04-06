826620991d090072756c2e50222f0f2b7d4376f1
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-04-06 17:31:33 +0300

    using BookContext

--- src/components/Catalogue/Catalogue.js
+++ src/components/Catalogue/Catalogue.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { useBookContext } from '../../contexts/BookContext.js';
 import CatalogueItem from './CatalogueItem/CatalogueItem.js';
 // TODO: the whole catalogue is not centered and is driving me nuts but here i am writing todos :)
-const Catalogue = ({ books }) => {
+const Catalogue = () => {
+    const { books } = useBookContext();
     return (
         <section id="catalogue-page">
             <h1 style={styles.heading}>All Posts</h1>
-            <div id="card-container" style={styles.cardContainer}>
-                {books.map(x =>
-                    <CatalogueItem key={x._id} {...x} />
-                )}
-                {books.length === 0 && (
-                    <h3 style={styles.hThree} className="no-articles">No posts yet</h3>
-                )}
+            <div id="card-container" className='container text-center' style={styles.cardContainer}>
+                <div className='row'>
+                    {books.map(x =>
+                        <CatalogueItem key={x._id} {...x} />
+                    )}
+                    {books.length === 0 && (
+                        <h3 style={styles.hThree} className="no-articles">No posts yet</h3>
+                    )}
+                </div>
             </div>
         </section>
     );
@@ -26,7 +30,7 @@ const styles = {
         // padding: "5%",
         marginLeft: "12%",
         marginRight: "auto",
-
+        marginBottom: "4%",
         margin: "auto",
         // width: "90%",
     },
