04a3753bcae3c6ee1404d9ecd9af5b78bfce4de3
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-04-06 17:32:55 +0300

    added CreateFormKeys

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,75 +1,79 @@
-import { useState } from "react";
+import { useBookContext } from '../../contexts/BookContext.js';
+import { useForm } from "../../hooks/useForm.js";
 
-const Create = ({ onCreateSubmit }) => {
-    const [values, setValues] = useState({
-        title: "",
-        author: "",
-        imageUrl: "",
-        summary: "",
-        genre: "",
-        favouriteQuote: "",
-        year: "",
-        tropes: "",
-        pages: "",
-    });
-    const onChangeHandler = (e) => {
-        setValues(state => ({ ...state, [e.target.name]: e.target.value }));
-    };
+const CreateFormKeys = {
+    Title: "title",
+    Author: "author",
+    ImageUrl: "imageUrl",
+    Summary: "summary",
+    Genre: "genre",
+    FavouriteQuote: "favouriteQuote",
+    Year: "year",
+    Tropes: "tropes",
+    Pages: "pages",
+};
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        console.log(values);
-        onCreateSubmit(values);
-    };
+export const Create = () => {
+    const { onCreateSubmit } = useBookContext();
+    const { values, changeHandler, onSubmit } = useForm({
+        [CreateFormKeys.Title]: "",
+        [CreateFormKeys.Author]: "",
+        [CreateFormKeys.ImageUrl]: "",
+        [CreateFormKeys.Summary]: "",
+        [CreateFormKeys.Genre]: "",
+        [CreateFormKeys.FavouriteQuote]: "",
+        [CreateFormKeys.Year]: "",
+        [CreateFormKeys.Tropes]: "",
+        [CreateFormKeys.Pages]: "",
+    }, onCreateSubmit);
     return (
-        // TODO: borders + shadow on login, create, register forms (like the borders on the catalogue page)
         <div className='card border-4 rounded-3 mb-3' style={styles.mainContainer}>
-            <form className="row g-3" onSubmit={onSubmit} style={styles.form}>
+            <form className="row g-3" method="POST" onSubmit={onSubmit} style={styles.form}>
                 <h1 style={styles.heading}>CREATE A POST</h1>
 
                 <div className="col-md-6 p3">
                     <label htmlFor="inputTitlel4" className="form-label">Title</label>
-                    <input type="text" className="form-control" id="inputTitle4" value={values.title} name="title" onChange={onChangeHandler} placeholder="Little Women" />
+                    <input type="text" className="form-control" id="inputTitle4" value={values.title} name={CreateFormKeys.Title} onChange={changeHandler} placeholder="Little Women" />
                 </div>
                 <div className="col-md-6">
                     <label htmlFor="inputAuthor4" className="form-label">Author</label>
-                    <input type="text" className="form-control" id="inputAuthor4" value={values.author} name="author" onChange={onChangeHandler} placeholder="Louisa May Alcott" />
+                    <input type="text" className="form-control" id="inputAuthor4" value={values.author} name={CreateFormKeys.Author} onChange={changeHandler} placeholder="Louisa May Alcott" />
                 </div>
                 <div className="col-12">
                     <label htmlFor="inputImageUrl" className="form-label">Image URL</label>
-                    <input type="text" className="form-control" id="inputImageUrl" value={values.imageUrl} name="imageUrl" onChange={onChangeHandler} placeholder="https://" />
+                    <input type="text" className="form-control" id="inputImageUrl" value={values.imageUrl} name={CreateFormKeys.ImageUrl} onChange={changeHandler} placeholder="https://" />
                 </div>
                 <div className="col-12">
                     <label htmlFor="inputSummary" className="form-label">Summary</label>
-                    <input type="text" className="form-control" id="inputSummary" value={values.summary} name="summary" onChange={onChangeHandler} />
+                    <input type="text" className="form-control" id="inputSummary" value={values.summary} name={CreateFormKeys.Summary} onChange={changeHandler} />
                 </div>
                 <div className="col-12">
                     <label htmlFor="inputState" className="form-label">Genre</label>
-                    <select id="inputState" className="form-select" value={values.genre} name="genre" onChange={onChangeHandler}>
-                        <option selected>Choose...</option>
-                        <option value="adventure">Adventure</option>
-                        <option value="childrensFiction">Children's Fiction</option>
-                        <option value="dystopian">Dystopian</option>
-                        <option value="fantasy">Fantasy</option>
-                        <option value="historicalFiction">Historical Fiction</option>
-                        <option value="horror">Horror</option>
-                        <option value="mystery">Mystery</option>
-                        <option value="romance">Romance</option>
-                        <option value="scienceFiction">Science Fiction</option>
-                        <option value="thriller">Thriller</option>
-                        <option value="tragedy">Tragedy</option>
-                        <option value="youngAdult">Young Adult</option>
-                        <option value="nonfiction">Nonfiction</option>
-                        <option value="other">Other</option>
+                    <select id="inputState" className="form-select" value={values.genre} name={CreateFormKeys.Genre} onChange={changeHandler}>
+                        <option value="choose">Choose...</option>
+                        <option value="Adventure">Adventure</option>
+                        <option value="Children's Fiction">Children's Fiction</option>
+                        <option value="Dystopian">Dystopian</option>
+                        <option value="Fantasy">Fantasy</option>
+                        <option value="Historical Fiction">Historical Fiction</option>
+                        <option value="Horror">Horror</option>
+                        <option value="Mystery">Mystery</option>
+                        <option value="Romance">Romance</option>
+                        <option value="Science Fiction">Science Fiction</option>
+                        <option value="Thriller">Thriller</option>
+                        <option value="Tragedy">Tragedy</option>
+                        <option value="Young Adult">Young Adult</option>
+                        <option value="Non-fiction">Nonfiction</option>
+                        <option value="Other">Other</option>
                     </select>
                 </div>
                 <div className="col-12">
                     <label htmlFor="inputFavouriteQuote" className="form-label">Favourite Quote</label>
-                    <input type="text" className="form-control" id="inputFavouriteQuote" value={values.favouriteQuote} name="favouriteQuote" onChange={onChangeHandler} placeholder="Conceit spoils the finest genius." />
+                    <input type="text" className="form-control" id="inputFavouriteQuote" value={values.favouriteQuote} name={CreateFormKeys.FavouriteQuote} onChange={changeHandler} placeholder="Conceit spoils the finest genius." />
                 </div>
                 <div className="col-12">
                     <label htmlFor="inputFavouriteQuote" className="form-label">Year of Publication</label>
-                    <input type="text" className="form-control" id="inputFavouriteQuote" value={values.year} name="year" onChange={onChangeHandler} placeholder="1868" />
+                    <input type="text" className="form-control" id="inputFavouriteQuote" value={values.year} name={CreateFormKeys.Year} onChange={changeHandler} placeholder="1868" />
                 </div>
 
                 {/* <div className="col-12">
@@ -78,11 +82,11 @@ const Create = ({ onCreateSubmit }) => {
             </div> */}
                 <div className="col-md-6">
                     <label htmlFor="inputTropes" className="form-label">Tropes</label>
-                    <input type="text" className="form-control" id="inputTropes" value={values.tropes} name="tropes" onChange={onChangeHandler} />
+                    <input type="text" className="form-control" id="inputTropes" value={values.tropes} name={CreateFormKeys.Tropes} onChange={changeHandler} />
                 </div>
                 <div className="col-md-6">
                     <label htmlFor="inputPages" className="form-label">Pages</label>
-                    <input type="text" className="form-control" id="inputPages" value={values.pages} name="pages" onChange={onChangeHandler} />
+                    <input type="text" className="form-control" id="inputPages" value={values.pages} name={CreateFormKeys.Pages} onChange={changeHandler} />
                 </div>
                 {/* TODO: how do you manage a radio button value? */}
                 {/* <div className="col-12" >
