d1c2749aa85a905cd33336c4ae9606a8039c1b35
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-04-09 01:27:32 +0300

    header fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,58 +1,61 @@
 import { NavLink } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from '../../contexts/AuthContext.js';
+import styles from './Header.module.css';
 
 const Header = () => {
     const { isAuthenticated, name } = useContext(AuthContext);
 
     return (
-        <nav className="navbar navbar-expand-lg">
-            <div className="container-fluid">
-                <NavLink className="navbar-brand" to="/"><img id="logo" src="../logo_transparentBackground.png" alt="Logo" /></NavLink>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+        <nav className={styles.navigation}>
+            <nav className="navbar navbar-expand-lg">
+                <div className="container-fluid">
+                    <NavLink className="navbar-brand" to="/"><img id="logo" src="../logo_transparentBackground.png" alt="Logo" /></NavLink>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-                <div className="collapse navbar-collapse" id="navbarText">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link active" to="/catalogue">Catalogue</NavLink>
-                        </li>
-                        {isAuthenticated && (
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <NavLink className="nav-link active" to="/create">Create</NavLink>
+                                <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
                             </li>
-                        )}
-                    </ul>
-                    <ul className="navbar-nav mb-2 mb-lg-0 navbar-right">
-                        {!isAuthenticated && (
-                            <li className="nav-item" id="login">
-                                <NavLink className="nav-link active" to="/login">Login</NavLink>
+                            <li className="nav-item">
+                                <NavLink className="nav-link active" to="/catalogue">Catalogue</NavLink>
                             </li>
-                        )}
-                        {!isAuthenticated && (
-                            <li className="nav-item" id="register">
-                                <NavLink className="nav-link active" to="/register">Register</NavLink>
-                            </li>
-                        )}
-                        {isAuthenticated && (
-                            <li>
-                                <NavLink className="nav-link active" to="#"> Hi, {name}</NavLink>
-                            </li>
-                        )}
-                        {isAuthenticated && (
-                            <li className="nav-item" id="logout">
-                                <NavLink className="nav-link active" to="/logout">Logout</NavLink>
-                            </li>
-                        )}
-                    </ul>
-                    <div className="navbar-nav navbar-right mb-2 mb-lg-0" id="navBarAuth">
+                            {isAuthenticated && (
+                                <li className="nav-item">
+                                    <NavLink className="nav-link active" to="/create">Create</NavLink>
+                                </li>
+                            )}
+                        </ul>
+                        <ul className="navbar-nav mb-2 mb-lg-0 navbar-right">
+                            {!isAuthenticated && (
+                                <li className="nav-item" id="login">
+                                    <NavLink className="nav-link active" to="/login">Login</NavLink>
+                                </li>
+                            )}
+                            {!isAuthenticated && (
+                                <li className="nav-item" id="register">
+                                    <NavLink className="nav-link active" to="/register">Register</NavLink>
+                                </li>
+                            )}
+                            {isAuthenticated && (
+                                <li>
+                                    <NavLink className="nav-link active" to="#"> Hi, {name}</NavLink>
+                                </li>
+                            )}
+                            {isAuthenticated && (
+                                <li className="nav-item" id="logout">
+                                    <NavLink className="nav-link active" to="/logout">Logout</NavLink>
+                                </li>
+                            )}
+                        </ul>
+                        <div className="navbar-nav navbar-right mb-2 mb-lg-0" id="navBarAuth">
+                        </div>
                     </div>
                 </div>
-            </div>
+            </nav>
         </nav>
     );
 };
