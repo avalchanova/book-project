6e42a1cecb5db6ecbee76b0f45858e7ec2e4833d
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-04-19 17:15:14 +0300

    deploying with firebase

--- src/services/commentServices.js
+++ src/services/commentServices.js
@@ -1,17 +1,21 @@
 import { requestFactory } from "./requester.js";
 
-const baseUrl = 'http://localhost:3030/data/comments';
+const host = process.env.NODE_ENV === 'development'
+    ? `http://localhost:3030`
+    : `http://localhost:3030`;
+
+const url = `${host}/data/commments`;
 const request = requestFactory();
 
 export const getAll = async (bookId) => {
     const searchQuery = encodeURIComponent(`bookId="${bookId}"`);
     const relationQuery = encodeURIComponent(`author=_ownerId:users`);
-    const result = await request.get(`${baseUrl}?where=${searchQuery}&load=${relationQuery}`);
+    const result = await request.get(`${url}?where=${searchQuery}&load=${relationQuery}`);
     const comments = Object.values(result);
     return comments;
 };
 
 export const create = async (bookId, comment) => {
-    const result = await request.post(baseUrl, { bookId, comment });
+    const result = await request.post(url, { bookId, comment });
     return result;
 };
