d3f31b9e6729034fd7d2d972696e123161a232b3
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-04-19 17:15:09 +0300

    deploying with firebase

--- src/services/bookService.js
+++ src/services/bookService.js
@@ -1,28 +1,35 @@
 import { requestFactory } from './requester.js';
 
-const baseUrl = `http://localhost:3030/data/books`;
+console.log(process.env.NODE_ENV); // comes from nodejs
+// there are development environment and production environment
+
+const host = process.env.NODE_ENV === 'development'
+    ? `http://localhost:3030`
+    : `http://localhost:3030`;
+
+const url = `${host}/data/books`;
 
 export const bookServiceFactory = (token) => {
     const request = requestFactory(token);
     const getAll = async () => {
-        const result = await request.get(baseUrl);
+        const result = await request.get(url);
         const books = Object.values(result);
         return books;
     };
 
     const getOne = async (bookId) => {
-        const result = await request.get(`${baseUrl}/${bookId}`);
+        const result = await request.get(`${url}/${bookId}`);
         return result;
     };
 
     const create = async (bookData) => {
-        const result = await request.post(baseUrl, bookData);
+        const result = await request.post(url, bookData);
         return result;
     };
 
-    const edit = (bookId, data) => request.put(`${baseUrl}/${bookId}`, data);
+    const edit = (bookId, data) => request.put(`${url}/${bookId}`, data);
 
-    const deleteBook = (bookId) => request.delete(`${baseUrl}/${bookId}`);
+    const deleteBook = (bookId) => request.delete(`${url}/${bookId}`);
 
 
     return {
